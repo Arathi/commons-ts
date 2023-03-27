574c7928c0e717946d530750f8e99003ea30de53
Arathi <ssdfa7@gmail.com>
2023-03-27 16:30:15 +0800

    编译时生成*.d.ts
    1. 移除index.html
    2. vita服务端绑定端口改为30327
    
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import path, { resolve } from 'path';
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        port: 30327,
+        // open: "/dist/commons.umd.js"
+    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/TmUsCommons.ts'),
